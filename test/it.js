df1690a84374aa98b865b596fdc5b8cd13587ddb
Haim Weitman <weithaim@gmail.com>
2018-07-13 16:00:25 +0300

    IT

--- test/it.js
+++ test/it.js
@@ -3,12 +3,12 @@ const app = require('../server.js');
 const server = 'http://localhost:3000';
 const {describe, it} = require('mocha');
 const chai = require('chai');
-const chaiHttp = require('chai-http')
+const chaiHttp = require('chai-http');
 const expect = chai.expect;
 chai.use(chaiHttp);
 
-describe('Integration test', function(done) {
-    it('...', async () => {
+describe('Integration test', function() {
+    it('...', (done) => {
         chai.request(server)
             .post("/calculate")
             .type('application/json')
@@ -17,6 +17,19 @@ describe('Integration test', function(done) {
                 expect(err).to.be.null;
                 expect(res).to.have.status(200);
                 expect(res.body.display).to.equal("1");
+                done();
+            });
+    });
+
+    it('test2', (done) => {
+        chai.request(server)
+            .post("/calculate")
+            .type('application/json')
+            .send({calculatorState: {expression: "1", display: "1", clearDisplay: false, clearExpression: false}, input: "1"})
+            .end((err, res) => {
+                expect(err).to.be.null;
+                expect(res).to.have.status(200);
+                expect(res.body.display).to.equal("11");
                 done();
             });
     })
