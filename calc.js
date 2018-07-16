61c32cb1e1464aa9e24fd3273743e798f390568a
haim <weithaim@gmail.com>
2018-07-17 00:43:58 +0300

    fixed calculator - edge cases.

--- calc.js
+++ calc.js
@@ -1,22 +1,21 @@
 function calculateNextState(jsonState, input) {
-    let calc = jsonState ? JSON.parse(jsonState) : {expression: "", display: ""};
+    let calc = jsonState ? JSON.parse(jsonState) : {expression: "0", display: ""};
     if (isNaN(input)) {
         if (input === "=") {
-            calc.expression = calc.expression ? eval(calc.expression) + "" : "";
+            calc.expression = eval(calc.expression) + "";
             calc.display = calc.expression;
             calc.clearExpression = true;
-        } else if (!calc.expression && (input === "/" || input === "*")) {
-            return JSON.stringify(calc);
         } else {
             if (calc.clearDisplay && !calc.clearExpression) {
-                calc.expression = calc.expression ? eval(calc.expression.slice(0, -1)) + input : input;
+                calc.expression = eval(calc.expression.slice(0, -1)) + input;
             } else {
-                calc.expression = calc.expression ? eval(calc.expression) + input : input;
+                calc.expression = eval(calc.expression) + input;
             }
             calc.clearExpression = false;
         }
         calc.clearDisplay = true;
     } else {
+        calc.expression = calc.expression === "0" ? "" : calc.expression;
         calc.expression = !calc.clearExpression ? calc.expression + input : input;
         calc.display = !calc.clearDisplay ? calc.display + input : input;
         calc.clearDisplay = false;
