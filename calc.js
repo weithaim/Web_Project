5f1b2e0f1adf70b57e9d7b334d8fe2a55dbd2a24
haim <weithaim@gmail.com>
2018-07-16 17:57:30 +0300

    deleting redundant comments

--- calc.js
+++ calc.js
@@ -23,45 +23,4 @@ function calculateNextState(jsonState, input) {
     return JSON.stringify(calc);
 }
 
-
-// let s = null;
-// s = calculateNextState(s, "1");
-// console.log(JSON.parse(s).display); // 1
-// s = calculateNextState(s, "2");
-// console.log(JSON.parse(s).display); // 12
-// s = calculateNextState(s, "+");
-// console.log(JSON.parse(s).display); // 12
-// s = calculateNextState(s, "4");
-// console.log(JSON.parse(s).display); // 4
-// s = calculateNextState(s, "3");
-// console.log(JSON.parse(s).display); // 43
-// s = calculateNextState(s, "=");
-// console.log(JSON.parse(s).display); // 55
-// s = calculateNextState(s, "+");
-// console.log(JSON.parse(s).display); // 55
-// s = calculateNextState(s, "1");
-// console.log(JSON.parse(s).display); // 1
-// s = calculateNextState(s, "=");
-// console.log(JSON.parse(s).display); // 56
-// s = calculateNextState(s, "5");
-// console.log(JSON.parse(s).display); // 5
-// s = calculateNextState(s, "+");
-// console.log(JSON.parse(s).display); // 5
-// s = calculateNextState(s, "1");
-// console.log(JSON.parse(s).display); // 1
-// s = calculateNextState(s, "=");
-// console.log(JSON.parse(s).display); // 6
-// s = calculateNextState(s, "8");
-// console.log(JSON.parse(s).display); // 8
-// s = calculateNextState(s, "8");
-// console.log(JSON.parse(s).display); // 88
-// s = calculateNextState(s, "+");
-// console.log(JSON.parse(s).display); // 88
-// s = calculateNextState(s, "1");
-// console.log(JSON.parse(s).display); // 1
-// s = calculateNextState(s, "2");
-// console.log(JSON.parse(s).display); // 12
-// s = calculateNextState(s, "=");
-// console.log(JSON.parse(s).display); // 100
-
 module.exports = calculateNextState;
